2e7cf694b5dddfe4485d85bd202d324d4924af81
Truong Hoang Phuc <truonghoangphuc84@gmail.com>
2025-07-28 11:29:25 +0700

    Disable Copy

--- metropole_phumy_web/metropole_phumy/src/app/[locale]/[[...slug]]/page.client.tsx
+++ metropole_phumy_web/metropole_phumy/src/app/[locale]/[[...slug]]/page.client.tsx
@@ -6,7 +6,23 @@ const PageClient: React.FC<{ className: string }> = (props) => {
 
   useEffect(() => {
     if (window) {
-      window.scrollTo(0, 0)
+      window.scrollTo(0, 0);
+      document.documentElement.classList.add('loaded');
+      document.addEventListener('copy', function(e) {
+        e.preventDefault(); // Prevent the default copy action
+      });
+
+      document.addEventListener('cut', function(e) {
+        e.preventDefault(); // Prevent the default cut action
+      });
+
+      document.addEventListener('paste', function(e) {
+        e.preventDefault(); // Prevent the default paste action
+      });
+
+      document.addEventListener('contextmenu', function(e) {
+        e.preventDefault(); // Prevent the default right-click context menu
+      });
     }
   }, [props.className])
 
