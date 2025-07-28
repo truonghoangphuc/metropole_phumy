6bac40fde95145f1cc1962b2c0f5f45f244d30ba
Truong Hoang Phuc <truonghoangphuc84@gmail.com>
2025-07-28 11:33:56 +0700

    Change domain

--- metropole_phumy_web/metropole_phumy/src/utilities/constant.ts
+++ metropole_phumy_web/metropole_phumy/src/utilities/constant.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.API_DOMAIN || 'https://api.thehuntergroup.asia';
+const API_URL = process.env.API_DOMAIN || 'https://admin.metropolephumy.com';
 const rootURL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 
 export { API_URL, rootURL };
